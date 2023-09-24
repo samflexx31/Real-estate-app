1613b936d949eb76110d5d200278803083206e76
samflexx31 <samflex3124@gmail.com>
2023-09-24 08:49:43 +0100

    Hero

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Component/Header/Header";
+import Hero from "./Component/Hero/Hero";
+import "./App.css";
+import Companies from "./Component/Companies/Companies";
+import Residencies from "./Component/Residencies/Residencies";
+import Value from "./Component/Value/Value";
+import Contact from "./Component/Contact/Contact";
+import GetStarted from "./Component/GetStarted/GetStarted";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
